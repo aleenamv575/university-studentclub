2742297f032faae0172d80ce5e74a0138726fb84
aleenamv575 <89600878+aleenamv575@users.noreply.github.com>
2021-12-16 12:42:29 +0530

    Add files via upload

---
+++ fun.js
@@ -0,0 +1,18 @@
+function student()
+{
+    var x = document.getElementById("student");
+    var y=document.getElementById("teacher");
+
+     x.style.display = "block";
+     y.style.display="none";
+   
+}
+function teacher()
+{
+  var y = document.getElementById("teacher");
+  var x = document.getElementById("student");
+   
+     y.style.display = "block";
+     x.style.display = "none";
+   
+}
